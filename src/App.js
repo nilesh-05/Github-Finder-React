d16323644ef7bf7b334fc24b95672538a965e85a
nilesh-05 <nilesh.s0506@gmail.com>
2020-09-07 21:17:22 +0530

    bug not resolved while changing app.js to functional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Users from './Components/users/Users'
@@ -9,86 +9,79 @@ import Alert from './Components/layouts/Alert'
 import about from './Components/pages/about'
 import User from "./Components/users/User";
 
-class App extends Component {
-	state = {
-		users: [],
-		loading: false,
-		alert: null,
-		user: {},
-		repos: []
-	}
+const App = () => {
+	const [users, setUsers] = useState([])
+	const [user, setUser] = useState({})
+	const [repos, setRepos] = useState([])
+	const [loading, setLoading] = useState(false)
+	const [alert, setAlert] = useState(null)
 
-
-	async componentDidMount() {
-		this.setState({ loading: true })
-		const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-		this.setState({ users: res.data, loading: false })
-	}
-
-
-	searchUsers = async (text) => {
-		this.setState({ loading: true })
+	const searchUsers = async (text) => {
+		setLoading(true)
 		const res = await axios.get(`https://api.github.com/search/users?q=${text}&?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-		this.setState({ users: res.data.items, loading: false })
+		setUsers(res.data.item)
+		setLoading(false)
 	}
 
 
-	clearUsers = () => {
-		this.setState({ users: [], loading: false })
+	const clearUsers = () => {
+		setUsers([])
+		setLoading(false)
 	}
 
 
-	setAlert = (msg, type) => {
-		this.setState({ alert: { msg: msg, type: type } })
+	const showAlert = (msg, type) => {
+		setAlert({ msg, type })
 		setTimeout(() => {
-			this.setState({ alert: null })
+			setAlert(null)
 		}, 4000)
 	}
 
 	//get a single github user
-	getUser = async (username) => {
-		this.setState({ loading: true })
+	const getUser = async (username) => {
+		setLoading(true)
 		const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-		this.setState({ user: res.data, loading: false })
+		setUser(res.data)
+		setLoading(false)
 	}
 
 	//get repos
-	getUserRepos = async (username) => {
-		this.setState({ loading: true })
+	const getUserRepos = async (username) => {
+		setLoading(true)
 		const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-		this.setState({ repos: res.data, loading: false })
+		setRepos(res.data)
+		setLoading(false)
 	}
 
 
-	render() {
-		const { users, loading, user, repos } = this.state
-		return (
-			<Router>
-				<div className="App">
-					<Navbar />
-					<div className="container">
-						<Alert alert={this.state.alert} />
-						<Switch>
-							<Route exact path='/' render={props => (
-								<Fragment>
-									<Search
-										searchUsers={this.searchUsers}
-										clearUsers={this.clearUsers}
-										showClear={users.length > 0 ? true : false}
-										setAlert={this.setAlert}
-									/>
-									<Users loading={loading} users={users} />
-								</Fragment>
-							)}></Route>
-							<Route exact path='/about' component={about} />
-							<Route exact path='/user/:login' render={props => (
-								<User {...props} getUser={this.getUser} user={user} loading={loading} getUserRepos={this.getUserRepos} repos={repos} />
-							)} />
-						</Switch>
-					</div>
+	return (
+		<Router>
+			<div className="App">
+				<Navbar />
+				<div className="container">
+					<Alert alert={alert} />
+					<Switch>
+						<Route exact path='/' render={props => (
+							<Fragment>
+								<Search
+									searchUsers={searchUsers}
+									clearUsers={clearUsers}
+									showClear={users.length > 0 ? true : false}
+									setAlert={showAlert}
+								/>
+								<Users loading={loading} users={users} />
+							</Fragment>
+						)}>
+						</Route>
+						<Route exact path='/about' component={about} />
+						<Route exact path='/user/:login' render={props => (
+							<User {...props} getUser={getUser} user={user} loading={loading} getUserRepos={getUserRepos} repos={repos} />
+						)} />
+					</Switch>
 				</div>
-			</Router>
-		);
-	}
+			</div>
+		</Router>
+	);
 }
+
 export default App; 
--- src/Components/users/User.js
+++ src/Components/users/User.js
@@ -1,72 +1,74 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import Spinner from '../layouts/Spinner'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import Repos from '../repos/Repos'
 
-class User extends Component {
-	componentDidMount() {
-		this.props.getUser(this.props.match.params.login)
-		this.props.getUserRepos(this.props.match.params.login)
-	}
-	static propTypes = {
-		loading: PropTypes.bool,
-		user: PropTypes.object.isRequired,
-		getUser: PropTypes.func.isRequired,
-		getUserRepos: PropTypes.func.isRequired,
-		repos: PropTypes.array.isRequired,
-	}
-	render() {
-		const { name, avatar_url, company, location, bio, blog, login, html_url, followers, following, public_repos, hireable } = this.props.user
-		const { loading, repos } = this.props
-		if (loading) return <Spinner />
-		return (
-			<Fragment>
-				<Link to='/' className='btn btn-light'>Back to search</Link>
+const User = ({ user, loading, repos, getUser, getUserRepos, match }) => {
+	useEffect(() => {
+		getUser(match.params.login)
+		getUserRepos(match.params.login)
+		// eslint-disable-next-line
+	}, [])
+
+	const { name, avatar_url, company, location, bio, blog, login, html_url, followers, following, public_repos, hireable } = user
+
+	if (loading) return <Spinner />
+
+	return (
+		<Fragment>
+			<Link to='/' className='btn btn-light'>Back to search</Link>
 				Hireable : {' '}
-				{hireable ?
-					<i className='fa fa-check text-success' /> : <i className='fa fa-times-circle text-danger' />}
-				<div className="card grid-2">
-					<div className="all-center">
-						<img src={avatar_url} className='round-img' style={{ width: '150px' }} alt="" />
-						<h1>{name}</h1>
-						<p>Based in : {location}</p>
-					</div>
-					<div>
-						{bio && (<Fragment>
-							<h3>Bio : </h3>
-							<p>{bio}</p>
-						</Fragment>
-						)}
-						<a href={html_url} className='btn btn-dark my-1'>Visit Github Profile</a>
-						<ul>
-							<li>
-								{login && (<Fragment>
-									<strong>Username : </strong>{login}
-								</Fragment>)}
-							</li>
-							<li>
-								{company && (<Fragment>
-									<strong>Company : </strong>{company}
-								</Fragment>)}
-							</li>
-							<li>
-								{blog && (<Fragment>
-									<strong>Website : </strong>{blog}
-								</Fragment>)}
-							</li>
-						</ul>
-					</div>
+			{hireable ?
+				<i className='fa fa-check text-success' /> : <i className='fa fa-times-circle text-danger' />}
+			<div className="card grid-2">
+				<div className="all-center">
+					<img src={avatar_url} className='round-img' style={{ width: '150px' }} alt="" />
+					<h1>{name}</h1>
+					<p>Based in : {location}</p>
 				</div>
-				<div className="card text-center">
-					<div className="badge badge-primary">Followers : {followers}</div>
-					<div className="badge badge-success">Following : {following}</div>
-					<div className="badge badge-light">Public Repos : {public_repos}</div>
+				<div>
+					{bio && (<Fragment>
+						<h3>Bio : </h3>
+						<p>{bio}</p>
+					</Fragment>
+					)}
+					<a href={html_url} className='btn btn-dark my-1'>Visit Github Profile</a>
+					<ul>
+						<li>
+							{login && (<Fragment>
+								<strong>Username : </strong>{login}
+							</Fragment>)}
+						</li>
+						<li>
+							{company && (<Fragment>
+								<strong>Company : </strong>{company}
+							</Fragment>)}
+						</li>
+						<li>
+							{blog && (<Fragment>
+								<strong>Website : </strong>{blog}
+							</Fragment>)}
+						</li>
+					</ul>
 				</div>
-				<Repos repos={repos} />
-			</Fragment>
-		)
-	}
+			</div>
+			<div className="card text-center">
+				<div className="badge badge-primary">Followers : {followers}</div>
+				<div className="badge badge-success">Following : {following}</div>
+				<div className="badge badge-light">Public Repos : {public_repos}</div>
+			</div>
+			<Repos repos={repos} />
+		</Fragment>
+	)
+}
+
+User.propTypes = {
+	loading: PropTypes.bool,
+	user: PropTypes.object.isRequired,
+	getUser: PropTypes.func.isRequired,
+	getUserRepos: PropTypes.func.isRequired,
+	repos: PropTypes.array.isRequired,
 }
 
 export default User
