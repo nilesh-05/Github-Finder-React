fcfd4bed8859c79c703f4e29f02a0d4398d5e5c1
nilesh-05 <nilesh.s0506@gmail.com>
2020-09-07 21:35:03 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
 	const searchUsers = async (text) => {
 		setLoading(true)
 		const res = await axios.get(`https://api.github.com/search/users?q=${text}&?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-		setUsers(res.data.item)
+		setUsers(res.data.items)
 		setLoading(false)
 	}
 
