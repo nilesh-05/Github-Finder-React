8cf729115cb6b0f13d56d051b57e82de9d5fa68e
nilesh-05 <nilesh.s0506@gmail.com>
2020-09-08 18:58:13 +0530

    completed with NotFound and the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Users from './Components/users/Users'
 import Navbar from './Components/layouts/Navbar'
-import Search from './Components/users/Search'
 import Alert from './Components/layouts/Alert'
 import about from './Components/pages/about'
 import User from "./Components/users/User";
 import GithubState from './context/github/GithubState'
 import AlertState from './context/alert/AlertState'
+import NotFound from './Components/pages/NotFound'
+import Home from './Components/pages/home'
+
 
 const App = () => {
 
@@ -21,15 +22,17 @@ const App = () => {
 						<div className="container">
 							<Alert />
 							<Switch>
-								<Route exact path='/' render={props => (
-									<Fragment>
-										<Search />
-										<Users />
-									</Fragment>
-								)}>
+								<Route
+									exact
+									path='/'
+									component={Home}>
 								</Route>
-								<Route exact path='/about' component={about} />
+								<Route
+									exact
+									path='/about'
+									component={about} />
 								<Route exact path='/user/:login' component={User} />
+								<Route component={NotFound} />
 							</Switch>
 						</div>
 					</div>
---
+++ src/Components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+	return (
+		<div>
+			<h1>Error 404 :(</h1>
+			<small >Page you are looking for does not exist.</small>
+		</div>
+	)
+}
+
+export default NotFound
